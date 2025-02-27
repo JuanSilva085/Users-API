fe5830526f3ab2f2242004db8fb09ed3dd91534b
JuanSilva085 <juansilva02042001@gmail.com>
2025-02-26 22:30:24 -0300

    Users.API

---
+++ Users.API/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Users.API.Data;
+using Users.API.Models;
+
+namespace Users.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+    {
+        return await _context.Usuarios.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Usuario>> GetUsuario(int id)
+    {
+        var usuario = await _context.Usuarios.FindAsync(id);
+        if (usuario == null)
+            return NotFound();
+        return usuario;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUsuario), new { id = usuario.Id }, usuario);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUsuario(int id, Usuario usuario)
+    {
+        if (id != usuario.Id)
+            return BadRequest("O ID do usuário na URL deve ser o mesmo do corpo da requisição.");
+
+        var usuarioExistente = await _context.Usuarios.FindAsync(id);
+        if (usuarioExistente == null)
+            return NotFound("Usuário não encontrado.");
+
+        usuarioExistente.Nome = usuario.Nome;
+        usuarioExistente.Email = usuario.Email;
+
+        _context.Entry(usuarioExistente).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUsuario(int id)
+    {
+        var usuario = await _context.Usuarios.FindAsync(id);
+        if(usuario == null)
+            return NotFound();
+
+        _context.Usuarios.Remove(usuario);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ Users.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users.API.Data;
+using Users.API.Models;
+
+namespace Users.API.Data;
+
+public class AppDbContext : DbContext
+{
+   public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+   
+  public DbSet<Usuario> Usuarios { get; set; }
+}
